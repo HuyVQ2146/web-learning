e599e2933a4136894998b677c0e8e99c49f233b5
Vu Quang Huy <Huy.VQ2421184@sis.hust.edu.vn>
2025-01-25 11:26:09 +0700

    learning basic

---
+++ JS-learning/index.js
@@ -0,0 +1,19 @@
+console.log("Hello"); //cái này sẽ hiện trong phần console trong inspect của web
+window.alert("This is an alert!"); //window.alert() và alert() không khác gì nhau
+window.alert("This is the second alert!"); //sau cái đầu thì sẽ hiện cnay
+//js cũng chạy theo thứ tự từ trên xuống dưới
+
+/*
+Đây là cách
+để comment nhiều dòng
+trong JavaScript
+*/
+document.getElementById('h1').textContent = 'Welcome to my website!'; //tạo chữ trên web bằng JS
+//document để chọn cái phần chữ trong web
+document.getElementById('p').textContent = 'This is a simple website created using HTML, CSS, and JavaScript.';
+
+let x;
+x = 100;
+console.log(x);
+let y  = 123;
+console.log(y);
